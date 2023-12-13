413218b0bbf8cfc5e2d54b8dc7fa7b2b5069c00e
chris <5sensprod@gmail.com>
2023-12-13 06:29:46 +0100

    nettoyage

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -18,11 +18,7 @@ export const ProductProvider = ({ children }) => {
   const [categories, setCategories] = useState([])
   const [products, setProducts] = useState([])
   const [baseUrl, setBaseUrl] = useState('')
-  const [isBulkEditActive, setIsBulkEditActive] = useState(false)
   const [selectedProducts, setSelectedProducts] = useState(new Set())
-  const [fieldsToEdit, setFieldsToEdit] = useState({})
-  const [showBulkEditForm, setShowBulkEditForm] = useState(false)
-  const [editingProduct, setEditingProduct] = useState(null)
 
   useEffect(() => {
     getApiBaseUrl().then((url) => {
@@ -40,9 +36,7 @@ export const ProductProvider = ({ children }) => {
         console.error('Erreur lors de la récupération des produits:', error)
       }
     }
-
     fetchProducts()
-
     let eventSource
     const setupSSE = () => {
       eventSource = new EventSource(`${baseUrl}/api/events`)
@@ -73,50 +67,8 @@ export const ProductProvider = ({ children }) => {
       const retrievedCategories = await getCategories()
       setCategories(retrievedCategories)
     }
-
     fetchCategories()
   }, [])
-
-  // Fonction pour activer/désactiver la modification en masse
-  const toggleBulkEdit = () => {
-    setIsBulkEditActive((prevState) => !prevState)
-    if (!isBulkEditActive) {
-      setSelectedProducts(new Set())
-    }
-  }
-
-  // Fonction pour gérer la sélection des produits
-  const handleProductSelect = (productId) => {
-    setSelectedProducts((prevSelected) => {
-      const newSelected = new Set(prevSelected)
-      if (newSelected.has(productId)) {
-        newSelected.delete(productId)
-      } else {
-        newSelected.add(productId)
-      }
-      return newSelected
-    })
-  }
-
-  // Fonction pour gérer la sélection des champs à modifier
-  const handleFieldSelect = (fieldName) => {
-    setFieldsToEdit((prevFields) => ({
-      ...prevFields,
-      [fieldName]: !prevFields[fieldName],
-    }))
-  }
-
-  const cancelEdit = () => {
-    setEditingProduct(null)
-  }
-
-  const cancelBulkEdit = () => {
-    setShowBulkEditForm(false)
-    setIsBulkEditActive(false)
-    setSelectedProducts(new Set())
-    setFieldsToEdit({})
-    // Autres actions nécessaires pour annuler l'édition en masse
-  }
 
   const addProductToContext = async (productData) => {
     try {
@@ -126,11 +78,10 @@ export const ProductProvider = ({ children }) => {
         prixVente: productData.prixVente
           ? parseFloat(productData.prixVente)
           : 0,
-        dateSoumission: new Date().toISOString(), // Ajoutez la date de soumission actuelle
+        dateSoumission: new Date().toISOString(),
       }
 
-      const response = await addProduct(formattedData) // Utilisation des données formatées
-      // Mettez à jour l'état du contexte si nécessaire, par exemple ajouter le produit à une liste
+      const response = await addProduct(formattedData)
       return response
     } catch (error) {
       console.error("Erreur lors de l'ajout du produit:", error)
@@ -165,28 +116,14 @@ export const ProductProvider = ({ children }) => {
   const contextValue = {
     searchTerm,
     setSearchTerm,
-    editingProduct,
-    setEditingProduct,
-    cancelEdit,
     selectedCategoryId,
     setSelectedCategoryId,
     categories,
     setCategories,
     products,
     baseUrl,
-    isBulkEditActive,
-    setIsBulkEditActive,
-    toggleBulkEdit,
-    selectedProducts,
-    handleProductSelect,
-    fieldsToEdit,
-    handleFieldSelect,
-    setFieldsToEdit,
     selectedProducts,
     setSelectedProducts,
-    showBulkEditForm,
-    setShowBulkEditForm,
-    cancelBulkEdit,
     setProducts,
     addProduct: addProductToContext,
     updateProductInContext,
