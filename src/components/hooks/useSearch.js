e81694c8f225d8877ff73fc53f31f4fc7e1e3b05
chris <5sensprod@gmail.com>
2023-12-13 09:20:04 +0100

    refacto

--- src/components/hooks/useSearch.js
+++ src/components/hooks/useSearch.js
@@ -1,56 +1,41 @@
 import { useMemo } from 'react'
 
+const matchesSearchTerm = (value, searchTerm) => {
+  return (
+    value && value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+  )
+}
+
+const matchesCategory = (product, selectedCategoryId) => {
+  return (
+    !selectedCategoryId ||
+    product.categorie === selectedCategoryId ||
+    product.sousCategorie === selectedCategoryId
+  )
+}
+
+const matchesSKU = (product, searchTerm) => {
+  if (Array.isArray(product.SKU)) {
+    return product.SKU.some(
+      (skuItem) =>
+        matchesSearchTerm(skuItem.diapason, searchTerm) ||
+        matchesSearchTerm(skuItem.gencode, searchTerm),
+    )
+  }
+  return matchesSearchTerm(product.SKU, searchTerm)
+}
+
 const useSearch = (products, searchTerm, selectedCategoryId) => {
-  const filteredProducts = useMemo(() => {
-    return products.filter((product) => {
-      const lowerCaseSearchTerm = searchTerm.toString().toLowerCase()
-
-      // Assurez-vous que les propriétés sont des chaînes de caractères avant d'appeler toLowerCase()
-      const referenceIncludes =
-        product.reference &&
-        product.reference.toLowerCase().includes(lowerCaseSearchTerm)
-      const marqueIncludes =
-        product.marque &&
-        product.marque.toLowerCase().includes(lowerCaseSearchTerm)
-
-      const gencodeIncludes = product.gencode
-        ? product.gencode.toString().includes(lowerCaseSearchTerm)
-        : false
-
-      const categoryMatches = selectedCategoryId
-        ? product.categorie === selectedCategoryId ||
-          product.sousCategorie === selectedCategoryId
-        : true
-      let skuArrayIncludes = false
-      if (Array.isArray(product.SKU)) {
-        skuArrayIncludes = product.SKU.some((skuItem) => {
-          const diapasonIncludes =
-            skuItem.diapason &&
-            skuItem.diapason.toLowerCase().includes(lowerCaseSearchTerm)
-          const skuGencodeIncludes = skuItem.gencode
-            ? skuItem.gencode.toString().includes(lowerCaseSearchTerm)
-            : false
-
-          return diapasonIncludes || skuGencodeIncludes
-        })
-      }
-
-      const skuStringIncludes =
-        typeof product.SKU === 'string' &&
-        product.SKU.includes(lowerCaseSearchTerm)
-
-      return (
-        categoryMatches &&
-        (referenceIncludes ||
-          marqueIncludes ||
-          gencodeIncludes ||
-          skuArrayIncludes ||
-          skuStringIncludes)
-      )
-    })
+  return useMemo(() => {
+    return products.filter(
+      (product) =>
+        matchesCategory(product, selectedCategoryId) &&
+        (matchesSearchTerm(product.reference, searchTerm) ||
+          matchesSearchTerm(product.marque, searchTerm) ||
+          matchesSearchTerm(product.gencode, searchTerm) ||
+          matchesSKU(product, searchTerm)),
+    )
   }, [products, searchTerm, selectedCategoryId])
-
-  return filteredProducts
 }
 
 export default useSearch
--- src/components/product/ProductManager.jsx
+++ src/components/product/ProductManager.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import useSearch from '../hooks/useSearch'
-import useWebSocketConnection from '../hooks/useWebSocketConnection'
-import useGlobalScannedDataHandler from '../hooks/useGlobalScannedDataHandler'
 import AddProductForm from './AddProductForm'
 import ProductTable from './ProductTable'
 import { getCategories } from '../../api/categoryService'
@@ -9,7 +7,7 @@ import SelectCategory from '../category/SelectCategory'
 import NoMatchButton from '../ui/NoMatchButton'
 import ProductSearch from './ProductSearch'
 import { useProductContext } from '../../contexts/ProductContext'
-import { Box, Button } from '@mui/material'
+import { Button } from '@mui/material'
 
 const ProductManager = () => {
   const {
@@ -43,23 +41,11 @@ const ProductManager = () => {
     filteredProducts.length === 0 &&
     isSearchingByReferenceOnly
 
-  const isAndroidWebView = navigator.userAgent.toLowerCase().includes('wv')
-
-  useGlobalScannedDataHandler(setSearchTerm)
-
-  useWebSocketConnection(setSearchTerm)
-
   useEffect(() => {
     if (searchTerm.trim() === '') {
       setShowAddProductForm(false)
     }
   }, [searchTerm])
-
-  const handleScanClick = () => {
-    if (window.Android && isAndroidWebView) {
-      window.Android.performScan()
-    }
-  }
 
   const handleProductSubmit = () => {
     setShowAddProductForm(false)
@@ -75,56 +61,54 @@ const ProductManager = () => {
     setSearchTerm('')
   }
 
+  const renderProductTableOrMessage = () => {
+    if (filteredProducts.length > 0) {
+      return <ProductTable products={filteredProducts} />
+    }
+
+    return (
+      <div>
+        {showAddProductForm ? (
+          <p>Ajouter un nouveau produit</p>
+        ) : (
+          <p>Aucun produit trouvé.</p>
+        )}
+        <NoMatchButton
+          show={!showAddProductForm && showAddProductButton}
+          buttonText="Ajouter"
+          onClick={handleShowAddForm}
+        />
+      </div>
+    )
+  }
+
+  const renderAddProductForm = () => {
+    if (!showAddProductForm) return null
+
+    return (
+      <>
+        <AddProductForm
+          initialGencode={isGencode ? searchTerm : ''}
+          initialReference={!isGencode ? searchTerm : ''}
+          onProductAdd={handleProductSubmit}
+        />
+        <Button variant="contained" onClick={handleCancel}>
+          Annuler
+        </Button>
+      </>
+    )
+  }
+
   return (
     <div>
-      <Box />
-      {isAndroidWebView && (
-        <Button variant="contained" onClick={handleScanClick}>
-          Scanner un code-barres
-        </Button>
-      )}
-
-      <>
-        <ProductSearch />
-        <SelectCategory
-          categories={categories}
-          selectedCategoryId={selectedCategoryId}
-          onCategoryChange={(e) => setSelectedCategoryId(e.target.value)}
-        />
-      </>
-
-      <>
-        {filteredProducts.length > 0 ? (
-          <ProductTable products={filteredProducts} />
-        ) : (
-          <div>
-            {showAddProductForm ? (
-              <p>Ajouter un nouveau produit</p>
-            ) : (
-              <p>Aucun produit trouvé.</p>
-            )}
-            <NoMatchButton
-              show={!showAddProductForm && showAddProductButton}
-              buttonText="Ajouter"
-              onClick={handleShowAddForm}
-            />
-          </div>
-        )}
-        {showAddProductForm && (
-          <>
-            <AddProductForm
-              initialGencode={isGencode ? searchTerm : ''}
-              initialReference={!isGencode ? searchTerm : ''}
-              onProductAdd={handleProductSubmit}
-            />
-            <Button variant="contained" onClick={handleCancel}>
-              Annuler
-            </Button>
-          </>
-        )}
-      </>
-
-      <Box />
+      <ProductSearch />
+      <SelectCategory
+        categories={categories}
+        selectedCategoryId={selectedCategoryId}
+        onCategoryChange={(e) => setSelectedCategoryId(e.target.value)}
+      />
+      {renderProductTableOrMessage()}
+      {renderAddProductForm()}
     </div>
   )
 }
