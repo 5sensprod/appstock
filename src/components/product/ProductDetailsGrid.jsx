92b10141bc8dea48a8bd7760dcc571e67f5a2782
chris <5sensprod@gmail.com>
2023-12-16 09:33:34 +0100

    ajout style

--- src/components/product/ProductDetailsGrid.jsx
+++ src/components/product/ProductDetailsGrid.jsx
@@ -1,28 +1,78 @@
-import React from 'react'
+import React, { useMemo, useEffect, useRef, useCallback } from 'react'
 import { AgGridReact } from 'ag-grid-react'
 import 'ag-grid-community/dist/styles/ag-grid.css'
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css'
+import 'ag-grid-community/dist/styles/ag-theme-material.css'
 import { useProductContext } from '../../contexts/ProductContext'
+import { filterAndFormatProducts } from '../../utils/productUtils'
+import frenchLocale from '../locales/frenchLocale'
 
 const ProductDetailsGrid = ({ productIds }) => {
-  const { products } = useProductContext()
+  const { products, categories } = useProductContext()
 
-  const filteredProducts = products.filter((product) =>
-    productIds.includes(product._id),
+  const filteredProducts = filterAndFormatProducts(
+    products,
+    productIds,
+    categories,
   )
 
+  const gridApi = useRef(null)
+
+  const autoSizeStrategy = useMemo(() => {
+    return {
+      type: 'fitGridWidth',
+    }
+  }, [])
+
+  const onGridReady = useCallback((params) => {
+    gridApi.current = params.api
+    setTimeout(() => params.api.sizeColumnsToFit(), 100)
+
+    window.addEventListener('resize', () => {
+      setTimeout(() => params.api.sizeColumnsToFit(), 100)
+    })
+  }, [])
+
+  useEffect(() => {
+    if (gridApi.current) {
+      gridApi.current.sizeColumnsToFit()
+    }
+  }, [filteredProducts])
+
   const columnDefs = [
-    { field: 'reference', headerName: 'Référence' },
-    { field: 'prixVente', headerName: 'Prix de Vente' },
-    // ...autres champs selon les détails de vos produits...
+    { field: 'reference', headerName: 'Référence', resizable: true },
+    { field: 'prixVente', headerName: 'Prix Vente', resizable: true },
+    // { field: 'descriptionCourte', headerName: 'Description Courte', resizable: true },
+    { field: 'prixAchat', headerName: "Prix d'Achat", resizable: true },
+    { field: 'stock', headerName: 'Stock', resizable: true },
+    { field: 'tva', headerName: 'TVA', resizable: true },
+    { field: 'marque', headerName: 'Marque', resizable: true },
+    { field: 'categorie', headerName: 'Catégorie', resizable: true },
+    { field: 'sousCategorie', headerName: 'Sous-Catégorie', resizable: true },
+    { field: 'dateSoumission', headerName: 'Date Ajout', resizable: true },
+    // Ajoutez la propriété resizable à d'autres champs selon les détails de vos produits
   ]
 
+  const onFirstDataRendered = (params) => {
+    if (autoSizeStrategy.type === 'fitGridWidth') {
+      const allColumnIds = params.columnApi
+        .getAllColumns()
+        .map((column) => column.colId)
+      params.columnApi.autoSizeColumns(allColumnIds)
+    }
+  }
   return (
-    <div className="ag-theme-alpine" style={{ height: 400, width: '100%' }}>
+    <div
+      className="ag-theme-material"
+      style={{ height: 400, width: '100%', minWidth: 1024 }}
+    >
       <AgGridReact
+        localeText={frenchLocale}
         rowData={filteredProducts}
         columnDefs={columnDefs}
+        onGridReady={onGridReady}
+        onFirstDataRendered={onFirstDataRendered}
         domLayout="autoHeight"
+        autoSizeStrategy={autoSizeStrategy}
       />
     </div>
   )
--- src/utils/productUtils.js
+++ src/utils/productUtils.js
@@ -1,3 +1,5 @@
+import { format } from 'date-fns'
+
 export const createCategoryMap = (categories) => {
   return categories.reduce((acc, category) => {
     acc[category._id] = category.name
@@ -12,5 +14,22 @@ export const formatProducts = (products, categoryMap) => {
     id: product._id,
     categorie: categoryMap[product.categorie] || 'Non Catégorisé',
     sousCategorie: categoryMap[product.sousCategorie] || '',
+    dateSoumission: product.dateSoumission
+      ? format(new Date(product.dateSoumission), 'dd/MM/yyyy')
+      : '',
+    // ... autres champs ...
   }))
+}
+
+// Nouvelle fonction pour filtrer et formater les produits
+export const filterAndFormatProducts = (products, productIds, categories) => {
+  const categoryMap = createCategoryMap(categories)
+
+  // Filtrer les produits
+  const filteredProducts = products.filter((product) =>
+    productIds.includes(product._id),
+  )
+
+  // Appliquer la fonction formatProducts aux produits filtrés
+  return formatProducts(filteredProducts, categoryMap)
 }
