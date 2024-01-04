b79ef1a242cd07562a30e44b3233ef5ee9502aff
chris <5sensprod@gmail.com>
2024-01-04 14:19:19 +0100

    correction

--- src/components/PRODUCTS/ProductsGrid.jsx
+++ src/components/PRODUCTS/ProductsGrid.jsx
@@ -139,7 +139,7 @@ const ProductsGrid = ({ selectedCategoryId }) => {
   return (
     <DataGridPro
       localeText={frFR.components.MuiDataGrid.defaultProps.localeText}
-      rows={products}
+      rows={filteredProducts}
       columns={columns}
       initialState={{
         pagination: {
