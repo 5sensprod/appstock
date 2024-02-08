b41c74dbe6e6c9d8f2ed5e7d5abd95142184cd36
chris <5sensprod@gmail.com>
2024-02-08 15:55:19 +0100

    ajout champs

--- src/components/users/EditUser.jsx
+++ src/components/users/EditUser.jsx
@@ -13,7 +13,51 @@ const EditUser = ({ userInfo, onChange, onSubmit, onCancel }) => {
           onChange={onChange}
         />
       </div>
-      {/* Ajoutez d'autres champs ici */}
+      <div>
+        <label>Adresse:</label>
+        <input
+          type="text"
+          name="address"
+          value={userInfo.address || ''}
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <label>Ville:</label>
+        <input
+          type="text"
+          name="city"
+          value={userInfo.city || ''}
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input
+          type="text"
+          name="email"
+          value={userInfo.email || ''}
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <label>Téléphone:</label>
+        <input
+          type="text"
+          name="phone"
+          value={userInfo.phone || ''}
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <label>Numéro d'identification fiscale:</label>
+        <input
+          type="text"
+          name="taxId"
+          value={userInfo.taxId || ''}
+          onChange={onChange}
+        />
+      </div>
       <button type="submit">Enregistrer les modifications</button>
       <button type="button" onClick={onCancel}>
         Annuler
