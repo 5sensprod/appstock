14efb2b9c387e637a95163478f2491b999d3c639
chris <5sensprod@gmail.com>
2024-02-08 18:06:50 +0100

    ajout Recharts

--- src/components/charts/SalesBarChart.jsx
+++ src/components/charts/SalesBarChart.jsx
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { LineChart } from '@mui/x-charts'
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
 import { getInvoices } from '../../api/invoiceService'
 import moment from 'moment'
 
@@ -9,11 +18,10 @@ const SalesLineChart = () => {
   useEffect(() => {
     const fetchAndProcessInvoices = async () => {
       const invoices = await getInvoices()
-      console.log('Invoices:', invoices) // Log des factures reçues
 
       if (Array.isArray(invoices)) {
         const salesByDate = invoices.reduce((acc, invoice) => {
-          const date = moment(invoice.date).format('DD/MM/YYYY')
+          const date = moment(invoice.date).format('YYYY-MM-DD')
           if (!acc[date]) {
             acc[date] = 0
           }
@@ -21,12 +29,13 @@ const SalesLineChart = () => {
           return acc
         }, {})
 
-        const formattedData = Object.keys(salesByDate).map((date) => ({
-          date,
-          totalSales: salesByDate[date],
-        }))
+        const formattedData = Object.entries(salesByDate)
+          .map(([date, totalSales]) => ({
+            date,
+            totalSales,
+          }))
+          .sort((a, b) => new Date(a.date) - new Date(b.date))
 
-        console.log('Formatted Data:', formattedData) // Log des données formatées
         setSalesData(formattedData)
       }
     }
@@ -34,33 +43,35 @@ const SalesLineChart = () => {
     fetchAndProcessInvoices()
   }, [])
 
-  // Préparation des données pour les axes X et Y
-  const xAxisData = salesData.map((_, index) => index)
-  const yAxisData = salesData.map((item) => item.totalSales)
-
-  console.log('X Axis Data:', xAxisData) // Log des données de l'axe X
-  console.log('Y Axis Data:', yAxisData) // Log des données de l'axe Y
-
   return (
-    <LineChart
-      xAxis={[
-        {
-          data: xAxisData,
-          scaleType: 'time', // Ajout de scaleType ici
-          valueFormatter: (value) =>
-            salesData[value] ? salesData[value].date : '',
-        },
-      ]}
-      yAxis={[{ id: 'totalSalesAxis', scaleType: 'linear' }]}
-      series={[
-        {
-          yAxisKey: 'totalSalesAxis',
-          data: salesData.map((item) => item.totalSales),
-          label: 'Total Sales',
-        },
-      ]}
-      height={400}
-    />
+    <ResponsiveContainer width="100%" height={300}>
+      <LineChart
+        data={salesData}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          dataKey="date"
+          tickFormatter={(value) =>
+            moment(value, 'YYYY-MM-DD').format('DD/MM/YYYY')
+          }
+        />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="totalSales"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+      </LineChart>
+    </ResponsiveContainer>
   )
 }
 
--- src/components/pages/DashboardPage.jsx
+++ src/components/pages/DashboardPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-// import SalesLineChart from '../charts/SalesBarChart'
+import SalesLineChart from '../charts/SalesBarChart'
 import UserDetails from '../users/UserDetails'
 
 const DashboardPage = () => {
   return (
     <div>
       <UserDetails />
+      <SalesLineChart />
     </div>
   )
 }
