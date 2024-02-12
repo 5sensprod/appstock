2b8872f2f3c1e45ffd64091caa14f3b369cead9d
chris <5sensprod@gmail.com>
2024-02-12 16:49:43 +0100

    style

--- src/components/quote/useGeneratePdf.js
+++ src/components/quote/useGeneratePdf.js
@@ -1,23 +1,103 @@
+import React, { useContext } from 'react'
 import { jsPDF } from 'jspdf'
 import 'jspdf-autotable'
 import { getQuoteDetailsById } from '../../api/quoteService'
+import { CompanyInfoContext } from '../../contexts/CompanyInfoContext'
+import { formatPrice } from '../../utils/priceUtils'
 
 export const useGeneratePdf = () => {
+  const { companyInfo } = useContext(CompanyInfoContext)
+
+  // Calculer la date de validité (30 jours après la date du devis)
+
   const generatePdf = async (quoteId) => {
     try {
       const quoteDetails = await getQuoteDetailsById(quoteId)
       const doc = new jsPDF()
 
-      // En-tête du PDF
-      doc.setFontSize(16)
-      doc.text(`Devis Numéro: ${quoteDetails.quoteNumber}`, 10, 10)
+      const dateDuDevis = new Date(quoteDetails.date)
+      const validiteDevis = new Date(
+        dateDuDevis.getTime() + 30 * 24 * 60 * 60 * 1000,
+      )
+
+      let currentYPosition = 10
+
+      // Vérifier si companyInfo est disponible
+      if (companyInfo) {
+        doc.setFontSize(12)
+        doc.text(companyInfo.name.toUpperCase(), 10, currentYPosition)
+        currentYPosition += 5
+
+        doc.setFontSize(10)
+        doc.text(companyInfo.address, 10, currentYPosition)
+        currentYPosition += 5
+        doc.text(companyInfo.city, 10, currentYPosition)
+        currentYPosition += 5
+        doc.text(companyInfo.phone, 10, currentYPosition)
+        currentYPosition += 5
+        doc.text(companyInfo.email, 10, currentYPosition)
+        currentYPosition += 5
+        doc.text(companyInfo.taxId, 10, currentYPosition)
+        currentYPosition += 10
+      }
+
+      // Calculer la position X pour "DEVIS"
+      const devisText = 'DEVIS'
+      doc.setFontSize(18) // Taille de police plus grande pour "DEVIS"
+      const pageWidth = doc.internal.pageSize.getWidth()
+      const devisTextWidth = doc.getTextWidth(devisText)
+      const devisXPosition = pageWidth - devisTextWidth - 75 // 10 est la marge à droite
+
+      // Position Y ajustée pour centrer "DEVIS" verticalement par rapport aux infos de l'entreprise
+      const devisYPosition = currentYPosition - (5 * 5) / 2 // Exemple de calcul, ajustez selon votre mise en page
+
+      doc.text(devisText, devisXPosition, devisYPosition)
+
+      // En-tête du PDF ajusté pour éviter le chevauchement
+      doc.setFontSize(10)
+      doc.text(`N° : ${quoteDetails.quoteNumber}`, 10, currentYPosition)
+      currentYPosition += 5
+
+      doc.setFontSize(10)
+      doc.text(
+        `Valable jusqu'au : ${validiteDevis.toLocaleDateString()}`,
+        10,
+        currentYPosition,
+      )
+      currentYPosition += 5
+
       doc.setFontSize(12)
       doc.text(
-        `Date: ${new Date(quoteDetails.date).toLocaleDateString()}`,
+        `Date : ${new Date(quoteDetails.date).toLocaleDateString()}`,
         10,
-        20,
+        currentYPosition,
       )
-      doc.text(`Client: ${quoteDetails.customerInfo.name}`, 10, 30)
+      currentYPosition += 5
+
+      // Construction de la chaîne de caractères pour les informations du client
+      let clientInfoStr = `Client :`
+      const { name, email, phone } = quoteDetails.customerInfo
+      // Vérifie et affiche le nom s'il est disponible
+      if (name) {
+        doc.text(`Client : ${name}`, 10, currentYPosition)
+        currentYPosition += 5
+      }
+
+      // Vérifie et affiche l'email s'il est disponible
+      if (email) {
+        doc.text(`Email : ${email}`, 10, currentYPosition)
+        currentYPosition += 5
+      }
+
+      // Vérifie et affiche le téléphone s'il est disponible
+      if (phone) {
+        doc.text(`Téléphone : ${phone}`, 10, currentYPosition)
+        currentYPosition += 5
+      }
+
+      // Affichage des informations du client dans le PDF
+      doc.text(clientInfoStr, 10, currentYPosition)
+      currentYPosition += 5
 
       // Tableau des articles
       const itemHeaders = [
@@ -33,12 +113,12 @@ export const useGeneratePdf = () => {
 
       const items = quoteDetails.items.map((item) => ({
         reference: item.reference,
-        prixOriginal: `${item.prixOriginal} €`,
+        prixOriginal: formatPrice(item.prixOriginal), // Formaté
         quantity: item.quantity,
-        prixHT: `${item.prixHT} €`,
-        prixTTC: `${item.prixTTC} €`,
+        prixHT: formatPrice(item.prixHT), // Formaté
+        prixTTC: formatPrice(item.prixTTC), // Formaté
         tauxTVA: `${item.tauxTVA}%`,
-        totalTTCParProduit: `${item.totalTTCParProduit} €`,
+        totalTTCParProduit: formatPrice(item.totalTTCParProduit), // Formaté
         remiseMajorationValue:
           item.remiseMajorationLabel && item.remiseMajorationValue
             ? item.remiseMajorationLabel === 'Majoration'
@@ -49,22 +129,42 @@ export const useGeneratePdf = () => {
       console.log('Données pour le tableau des articles :', items)
 
       doc.autoTable({
-        startY: 40,
+        startY: (currentYPosition += 10),
         head: [itemHeaders.map((header) => header.title)],
         body: items.map((item) =>
           itemHeaders.map((header) => {
-            // Assurez-vous que la valeur est correctement traitée
             return item[header.dataKey]
           }),
         ),
+        headStyles: {
+          fillColor: 255,
+          textColor: 0,
+          lineColor: [50, 50, 50],
+        },
+        bodyStyles: {},
+        theme: 'striped',
       })
 
       // Récapitulatif des totaux
-      const finalY = doc.lastAutoTable.finalY + 10 // Position Y après le tableau
-      doc.setFontSize(12)
-      doc.text(`Total HT: ${quoteDetails.totalHT} €`, 10, finalY)
-      doc.text(`Total TTC: ${quoteDetails.totalTTC} €`, 10, finalY + 10)
+      const finalY = doc.lastAutoTable.finalY + 10
 
+      const rightMargin = 15
+
+      // Total HT
+      const totalHTText = `Total HT : ${formatPrice(quoteDetails.totalHT)}`
+      doc.setFontSize(10)
+      const totalHTTextWidth = doc.getTextWidth(totalHTText)
+      doc.text(totalHTText, pageWidth - totalHTTextWidth - rightMargin, finalY)
+
+      // Net à payer
+      const netAPayerText = `Net à payer : ${formatPrice(quoteDetails.totalTTC)}`
+      doc.setFontSize(12)
+      const netAPayerTextWidth = doc.getTextWidth(netAPayerText)
+      doc.text(
+        netAPayerText,
+        pageWidth - netAPayerTextWidth - rightMargin,
+        finalY + 7,
+      )
       // Si vous avez des remises ou majorations, vous pouvez les ajouter ici
 
       // Enregistrement du PDF
