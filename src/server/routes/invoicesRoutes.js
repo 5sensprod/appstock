c3605ac95f257063012101d842688254f6b060b8
chris <5sensprod@gmail.com>
2024-02-14 18:52:05 +0100

    modif ibdebt

--- src/server/routes/invoicesRoutes.js
+++ src/server/routes/invoicesRoutes.js
@@ -27,40 +27,71 @@ module.exports = (db) => {
   }
 
   // Ajouter une nouvelle facture
-  router.post('/', (req, res) => {
-    const dateTimeString = getDateTimeString()
-    const invoiceNumber = `${dateTimeString}`
+  router.post('/', async (req, res) => {
+    // Récupérer le dernier numéro de facture et incrémenter
+    db.invoices
+      .find({})
+      .sort({ invoiceNumber: -1 })
+      .limit(1)
+      .exec((err, lastInvoice) => {
+        if (err) {
+          res
+            .status(500)
+            .send(
+              'Erreur lors de la récupération du dernier numéro de facture.',
+            )
+          return
+        }
 
-    // Traitement des éléments pour s'assurer que les prix et les totaux sont des nombres flottants arrondis
-    let itemsFormatted = req.body.items.map((item) => ({
-      ...item,
-      quantite: parseInt(item.quantite, 10),
-      puHT: parseFloat(parseFloat(item.puHT).toFixed(2)),
-      puTTC: parseFloat(parseFloat(item.puTTC).toFixed(2)),
-      tauxTVA: parseFloat(item.tauxTVA),
-      totalItem: parseFloat(parseFloat(item.totalItem).toFixed(2)),
-      montantTVA: parseFloat(parseFloat(item.montantTVA).toFixed(2)),
-      remiseMajorationValue: parseFloat(item.remiseMajorationValue),
-    }))
+        let lastNumber = 0
+        if (lastInvoice.length > 0) {
+          // Extrait le numéro après le tiret et le convertit en nombre
+          const lastInvoiceNumber = lastInvoice[0].invoiceNumber.split('-')[1]
+          lastNumber = parseInt(lastInvoiceNumber, 10)
+        }
 
-    let newInvoice = {
-      ...req.body,
-      items: itemsFormatted,
-      totalHT: parseFloat(parseFloat(req.body.totalHT).toFixed(2)),
-      totalTVA: parseFloat(parseFloat(req.body.totalTVA).toFixed(2)),
-      totalTTC: parseFloat(parseFloat(req.body.totalTTC).toFixed(2)),
-      invoiceNumber,
-      date: new Date().toISOString(),
-    }
+        // Générer le nouveau numéro de facture
+        const newInvoiceNumber = `${new Date().getFullYear()}${String(new Date().getMonth() + 1).padStart(2, '0')}${String(new Date().getDate()).padStart(2, '0')}-${String(lastNumber + 1).padStart(6, '0')}`
 
-    db.invoices.insert(newInvoice, (err, invoice) => {
-      if (err) {
-        res.status(500).send("Erreur lors de l'ajout de la facture.")
-      } else {
-        res.status(201).json(invoice)
-      }
-    })
+        // Création de la nouvelle facture avec le nouveau numéro
+        let newInvoice = {
+          ...req.body,
+          invoiceNumber: newInvoiceNumber,
+          date: new Date().toISOString(),
+        }
+
+        // Traitement des éléments (gardé tel quel de votre exemple)
+        let itemsFormatted = req.body.items.map((item) => ({
+          ...item,
+          quantite: parseInt(item.quantite, 10),
+          puHT: parseFloat(parseFloat(item.puHT).toFixed(2)),
+          puTTC: parseFloat(parseFloat(item.puTTC).toFixed(2)),
+          tauxTVA: parseFloat(item.tauxTVA),
+          totalItem: parseFloat(parseFloat(item.totalItem).toFixed(2)),
+          montantTVA: parseFloat(parseFloat(item.montantTVA).toFixed(2)),
+          remiseMajorationValue: parseFloat(item.remiseMajorationValue),
+        }))
+
+        newInvoice.items = itemsFormatted
+        newInvoice.totalHT = parseFloat(parseFloat(req.body.totalHT).toFixed(2))
+        newInvoice.totalTVA = parseFloat(
+          parseFloat(req.body.totalTVA).toFixed(2),
+        )
+        newInvoice.totalTTC = parseFloat(
+          parseFloat(req.body.totalTTC).toFixed(2),
+        )
+
+        // Insérer la nouvelle facture dans la base de données
+        db.invoices.insert(newInvoice, (err, invoice) => {
+          if (err) {
+            res.status(500).send("Erreur lors de l'ajout de la facture.")
+          } else {
+            res.status(201).json(invoice)
+          }
+        })
+      })
   })
+
   // Mettre à jour une facture
   router.put('/:id', (req, res) => {
     const { id } = req.params
