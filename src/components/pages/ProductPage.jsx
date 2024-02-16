3d7d1a2dcef9d53bdafbeb3f1dfd9fb617424f38
chris <5sensprod@gmail.com>
2024-02-17 00:04:56 +0100

    fix

--- src/components/pages/ProductPage.jsx
+++ src/components/pages/ProductPage.jsx
@@ -5,27 +5,28 @@ import CategoryFilter from '../CATEGORIES/CategoryFilter'
 import { CategoryTreeSelectContext } from '../../contexts/CategoryTreeSelectContext'
 import ProductSearch from '../PRODUCTS/ProductSearch'
 import { useProductContextSimplified } from '../../contexts/ProductContextSimplified'
+import { Box } from '@mui/material'
 
 const ProductPage = () => {
   const { selectedCategory } = useContext(CategoryTreeSelectContext)
   const { searchTerm } = useProductContextSimplified()
 
   return (
-    <Grid container direction="column" spacing={1} mt={1}>
-      <Grid item xs={12} md={6}>
+    <Grid container direction="column" spacing={1} mt={2}>
+      <Box ml={1} mb={1}>
         <CategoryFilter />
-      </Grid>
+      </Box>
 
       {/* Intégration du composant ProductSearch */}
       {/* <ProductSearch /> */}
 
       {/* ProductsGrid peut maintenant utiliser searchTerm pour filtrer les produits */}
-      <Grid item xs={12} md={6}>
+      <Box ml={1}>
         <ProductsGrid
           selectedCategoryId={selectedCategory.categoryId}
           searchTerm={searchTerm}
         />
-      </Grid>
+      </Box>
     </Grid>
   )
 }
