949d996da75fd939a44badef99e63c2fc8bb02f1
chris <5sensprod@gmail.com>
2024-03-05 05:07:02 +0100

    ajout scrollbar

--- src/components/invoice/InvoiceConfirmationModal.jsx
+++ src/components/invoice/InvoiceConfirmationModal.jsx
@@ -27,6 +27,8 @@ const style = {
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
+  maxHeight: '80vh',
+  overflowY: 'auto',
 }
 import useHandlePayClick from '../../hooks/useHandlePayClick'
 
