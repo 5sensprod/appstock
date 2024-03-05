cc58061c86996c4dc4d25b3b11ab006852ccfad4
chris <5sensprod@gmail.com>
2024-03-05 09:37:10 +0100

    style

--- src/components/PRODUCTS/QRCodeGenerator.jsx
+++ src/components/PRODUCTS/QRCodeGenerator.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { QRCodeCanvas } from 'qrcode.react'
-import Barcode from 'react-barcode'
 import { useProductContextSimplified } from '../../contexts/ProductContextSimplified'
 import { jsPDF } from 'jspdf'
 import html2canvas from 'html2canvas'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
+import { formatPrice } from '../../utils/priceUtils'
+import Logo from '../ui/Logo'
 
 const QRCodeGenerator = ({ productId }) => {
   const { products } = useProductContextSimplified()
@@ -20,74 +21,85 @@ const QRCodeGenerator = ({ productId }) => {
 
   const generatePDF = async () => {
     const input = document.getElementById('printArea')
+    input.style.height = 'auto'
+
     const canvas = await html2canvas(input)
     const imgData = canvas.toDataURL('image/png')
 
     const pdf = new jsPDF({
       orientation: 'portrait',
+      unit: 'mm',
+      format: 'a4',
     })
 
-    const imgProps = pdf.getImageProperties(imgData)
-    const pdfWidth = pdf.internal.pageSize.getWidth()
-    const scaleFactor = 0.4
-    const pdfHeight =
-      ((imgProps.height * pdfWidth) / imgProps.width) * scaleFactor
+    // Dimensions pour l'étiquette
+    const labelWidth = 105
 
-    pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth * scaleFactor, pdfHeight)
-    pdf.save(`${product.reference}-label.pdf`)
+    // Calculer le ratio de l'image pour maintenir l'aspect
+    const imgRatio = canvas.height / canvas.width
+    const actualLabelHeight = labelWidth * imgRatio
+
+    // Pour aligner l'étiquette en haut à gauche
+    const xPosition = 0
+    const yPosition = 0
+
+    pdf.addImage(
+      imgData,
+      'PNG',
+      xPosition,
+      yPosition,
+      labelWidth,
+      actualLabelHeight,
+    )
+    pdf.save(`${product.reference}-etiquette.pdf`)
   }
 
   const printAreaStyle = {
-    width: '640px',
-    height: '360px',
+    position: 'relative',
+    width: '48%',
+    height: '25%',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f9f9f9',
+    backgroundColor: '#fff',
     border: '1px solid #ddd',
-    boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
     padding: '20px',
     margin: '20px auto',
-    borderRadius: '5px',
+  }
+
+  const logoStyle = {
+    position: 'absolute',
+    top: '1px', // Ajustez en fonction de l'endroit où vous voulez que le logo apparaisse
+    left: '1px', // Idem pour la position horizontale
+    // Autres styles spécifiques au logo...
   }
 
   return (
     <Box>
       <Box id="printArea" sx={printAreaStyle}>
-        <Typography variant="h5" component="h3">
-          {product.reference}
+        <Box sx={logoStyle}>
+          <Logo />{' '}
+          {/* Assurez-vous que le composant Logo accepte des styles via props ou a ses propres styles */}
+        </Box>
+        <Typography variant="h6">{product.reference}</Typography>
+        <Typography variant="h6" sx={{ mt: 2 }}>
+          {formatPrice(product.prixVente)}
         </Typography>
-        <Typography variant="body1" sx={{ mt: 2 }}>
-          Prix: {product.prixVente} €
-        </Typography>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-            gap: 2,
-          }}
-        >
-          <QRCodeCanvas value={gencode} size={100} />
-          <Barcode
-            value={gencode}
-            format="CODE128"
-            displayValue={false}
-            height={50}
-            width={1}
-          />
+        <Box>
+          <QRCodeCanvas value={gencode} size={50} />
         </Box>
       </Box>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={generatePDF}
-        sx={{ mt: 2 }}
-      >
-        Télécharger PDF
-      </Button>
+      <Box textAlign={'center'}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={generatePDF}
+          sx={{ mt: 2 }}
+        >
+          Générer
+        </Button>
+      </Box>
     </Box>
   )
 }
