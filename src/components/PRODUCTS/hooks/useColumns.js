32f44eb55a961e2d227379001454fa654e7ef083
chris <5sensprod@gmail.com>
2024-03-06 13:28:44 +0100

    nettoyage

--- src/components/PRODUCTS/hooks/useColumns.js
+++ src/components/PRODUCTS/hooks/useColumns.js
@@ -41,18 +41,6 @@ const useColumns = (
     return path.join(' > ')
   }
 
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  }
-
   const isNewRowFunction = (row) => {
     // Déterminez si la ligne est nouvelle. Par exemple, une ligne sans ID est considérée comme nouvelle.
     return !row._id
