27e0483a889db4733a7d2a685e7fa0802d312baf
chris <5sensprod@gmail.com>
2024-03-07 05:55:04 +0100

    fix chaine vide

--- src/utils/formatUtils.js
+++ src/utils/formatUtils.js
@@ -9,11 +9,19 @@ export const convertToNumber = (value, defaultValue = 0) => {
 }
 
 export const formatNumberWithComma = (number) => {
+  // Vérifie si "number" est une chaîne vide ou si ce n'est pas un nombre
+  if (number === '' || isNaN(number)) {
+    return '0' // Retourne '0' ou une autre valeur par défaut selon votre besoin
+  }
+
+  // Convertit "number" en nombre si ce n'est pas déjà le cas
+  const num = Number(number)
+
   // Vérifie si le nombre est un entier
-  if (Number.isInteger(number)) {
-    return number.toString()
+  if (Number.isInteger(num)) {
+    return num.toString()
   } else {
     // Pour les nombres avec des décimales, remplace le point par une virgule
-    return number.toString().replace('.', ',')
+    return num.toString().replace('.', ',')
   }
 }
