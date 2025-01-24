da0e119dcd6a3c24c708afb38c227a94173a3c62
chris <5sensprod@gmail.com>
2025-01-24 18:04:13 +0100

    Feat Amélioration des doubles REtour

--- src/main.js
+++ src/main.js
@@ -83,7 +83,7 @@ function setupAutoUpdater() {
           type: 'info',
           title: 'Mise à jour',
           message: 'Une mise à jour est disponible.',
-          buttons: ['Installer maintenant', 'Installer Plus tard'],
+          buttons: ['Installer maintenant', 'Plus tard'],
           defaultId: 0,
           cancelId: 1,
         })
